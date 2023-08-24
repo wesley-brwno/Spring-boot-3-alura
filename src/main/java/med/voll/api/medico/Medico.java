05e65e8d6270dc3cf7f7b0dccd696f6e179920e7
wesley <wesleybruno910@gmail.com>
2023-08-24 17:31:25 -0300

    Added classes related to persistence layer

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,24 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
