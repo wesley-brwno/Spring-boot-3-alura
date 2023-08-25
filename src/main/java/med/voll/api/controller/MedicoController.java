25d9bbdc7c0c6a1c65d256817e5b1cdb71f675e1
wesley <wesleybruno910@gmail.com>
2023-08-25 16:49:48 -0300

    Implementation of the funcionality list Medicos.
    The DTO DadosListagemMedicos  and the listagem method in MedicoController were created

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,24 +2,34 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadasrtoMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
-    @Autowired
-    private MedicoRepository repository;
+    private final MedicoRepository repository;
+
+    public MedicoController(MedicoRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadasrtoMedico dados) {
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public List<DadosListagemMedicos> listar() {
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    }
+
 }
