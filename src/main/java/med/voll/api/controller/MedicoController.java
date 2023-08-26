b3fa4e791a16ce32104bd8d12099a089dc128105
wesley <wesleybruno910@gmail.com>
2023-08-25 21:19:45 -0300

    Modification in the method listar
    The @PageableDeafault annotation was added with the properties size and sort

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +31,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedicos> listar(Pageable pageable) {
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
         return repository.findAll(pageable).map(DadosListagemMedicos::new);
     }
 }
