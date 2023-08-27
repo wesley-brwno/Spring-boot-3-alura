54b8abc0e7e5cd581d7f35b3fea0bdcf91c1491f
wesley <wesleybruno910@gmail.com>
2023-08-27 09:50:32 -0300

    Added id parameter to the record DadosListagemMedicos

--- src/main/java/med/voll/api/medico/DadosListagemMedicos.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -1,8 +1,8 @@
 package med.voll.api.medico;
 
-public record DadosListagemMedicos(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedicos(long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedicos (Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
