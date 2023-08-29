1bcec3bb2f878b598b7e134a2b2dff189ff570ff
wesley <wesleybruno910@gmail.com>
2023-08-29 19:33:16 -0300

    fix: correct the name of DadosEndereco to endereco in PacienteCadastroDTO.
    Changes the name of the field to endereco. This name is the one being received through the JSON.

--- src/main/java/med/voll/api/paciente/PacienteCadastroDTO.java
+++ src/main/java/med/voll/api/paciente/PacienteCadastroDTO.java
@@ -3,6 +3,7 @@ package med.voll.api.paciente;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
@@ -17,7 +18,8 @@ public record PacienteCadastroDTO(
         @NotBlank
         @Pattern(regexp = "\\d{11}")
         String cpf,
+        @NotNull
         @Valid
-        DadosEndereco dadosEndereco
+        DadosEndereco endereco
 ) {
 }
