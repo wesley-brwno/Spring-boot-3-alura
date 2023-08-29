cf48b81450855253f07fcc84b7ad012e0df91ffa
wesley <wesleybruno910@gmail.com>
2023-08-29 18:09:57 -0300

    feat: add V4__create-table-pacientes.sqlto create pacientes table in the databases.
    The patient table will store data related to Paciente entity

--- src/main/resources/db/migration/V4__create-table-pacientes.sql
+++ src/main/resources/db/migration/V4__create-table-pacientes.sql
@@ -2,8 +2,6 @@ create table pacientes (
     id bigint not null auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null unique,
-    crm varchar(6) not null unique,
-    especialidade varchar(100) not null,
     logradouro varchar(100) not null,
     bairro varchar(100) not null,
     cep varchar(9) not null,
@@ -13,5 +11,4 @@ create table pacientes (
     cidade varchar(100) not null,
 
     primary key(id)
-
 );
