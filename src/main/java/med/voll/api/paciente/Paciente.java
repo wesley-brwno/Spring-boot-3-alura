0393b960b0a095865894ea2a15d6bbe37fbbe344
wesley <wesleybruno910@gmail.com>
2023-08-29 14:50:36 -0300

    feat: add Paciente entirty class for represent a paciente in the database.
    Add the Paciente entity in the package med.voll.api.endereco. This class had these filds id, nome, telefone, CPF and endereco.

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,25 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity(name = "paciente")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String CPF;
+    @Embedded
+    private Endereco endereco;
+}
+
+
