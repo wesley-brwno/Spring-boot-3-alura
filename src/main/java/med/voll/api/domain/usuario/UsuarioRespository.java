bca7f9d032d5e57cdf4d58dbef326a68b7026926
wesley <wesleybruno910@gmail.com>
2023-08-31 19:49:25 -0300

    feat: add UsuarioRespository interface to enable CRUD operations
    Adds UsuarioRespository interface to the med.voll.api.domain.usuario package. This interface extends JpaRepository and enables CRUD operations to the Usuario Entity

---
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRespository.java
@@ -0,0 +1,6 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRespository extends JpaRepository<Usuario, Long> {
+}
