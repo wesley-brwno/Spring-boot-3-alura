f7f41b8dac0bd3e70ce337d2f207c5246e8e4d9a
wesley <wesleybruno910@gmail.com>
2023-08-31 10:53:48 -0300

    feat: add tratarError400() method, DadosErrorValidation record to TratadorDeErrors class
    The tratarError400() method is responsible for capturing the MethodArgumentNotValidException and returning a list of DadosErrorValidation object with filds campo and message and the 400 Bad Request response
    
    The DadosErrorValidation is a DTO record that represent an error message. It has two fields: campo and message. The campo field stores the name of the field that caused the error and, the messagem field stores the error message

--- src/main/java/med/voll/api/infra/TradadorDeErrors.java
+++ src/main/java/med/voll/api/infra/TradadorDeErrors.java
@@ -2,13 +2,30 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TradadorDeErrors {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> tratarError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErrorValidation>> tratarError400(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(DadosErrorValidation::new).toList());
+    }
+
+    private record DadosErrorValidation(String campo, String messagem) {
+        public DadosErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
