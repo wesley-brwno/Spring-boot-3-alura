910dc96d583dadaa7b2878c763745b21655b2458
wesley <wesleybruno910@gmail.com>
2023-09-01 11:53:06 -0300

    refec: inject TokenService to AutenticacaoController and modify efetuarloing() method
    Injects TokenService to AutenticacaoController in the package med.voll.api.controller
    
    The gerarToken() TokenService method accepts an Authentication argument. And return a token to be send in the HTTP response

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,7 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAtutenticacao;
-import org.springframework.beans.factory.annotation.Autowired;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,17 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
 
-    public final AuthenticationManager manager;
+    private final AuthenticationManager manager;
+    private final TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager manager) {
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService) {
         this.manager = manager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosAtutenticacao dados) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         Authentication authenticate = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authenticate.getPrincipal()));
     }
 
 }
