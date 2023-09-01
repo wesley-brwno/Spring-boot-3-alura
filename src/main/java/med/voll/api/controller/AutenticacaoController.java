a09b1a54080b27bb50c28a59830c74043e87eff2
wesley <wesleybruno910@gmail.com>
2023-09-01 13:22:52 -0300

    feat: add DadosTokenJWT DTO record to send JSON token
    Adds DadosTokenJWT to med.voll.api.infra.security package. This record allows to send a JSON response with the generated token in the efetuarLogin Rest POST method

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAtutenticacao;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -30,7 +31,8 @@ public class AutenticacaoController {
     public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosAtutenticacao dados) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         Authentication authenticate = manager.authenticate(token);
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authenticate.getPrincipal()));
+        String tokenJWT = tokenService.gerarToken((Usuario) authenticate.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 }
