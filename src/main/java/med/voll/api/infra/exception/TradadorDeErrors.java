987e1413b51bb0907a5f66038eb3c93f88f370b9
wesley <wesleybruno910@gmail.com>
2023-09-02 18:42:54 -0300

    feat: add methods to handle exceptions and return messages in the HTTP response to TratadorDeErrors class
    Adds methods to the TratadorDeErrors class to handle exceptions and return messages in the HTTP response. This allows for more consistent and user-friendly error handling

--- src/main/java/med/voll/api/infra/exception/TradadorDeErrors.java
+++ src/main/java/med/voll/api/infra/exception/TradadorDeErrors.java
@@ -1,13 +1,17 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.security.sasl.AuthenticationException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -22,6 +26,27 @@ public class TradadorDeErrors {
         List<FieldError> fieldErrors = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(fieldErrors.stream().map(DadosErrorValidation::new).toList());
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> tratarErro500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
 
     private record DadosErrorValidation(String campo, String messagem) {
         public DadosErrorValidation(FieldError error) {
