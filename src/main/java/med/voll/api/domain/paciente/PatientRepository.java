90a27eb6c260b41ee59b94b9821267e71a6e21c2
wesley <wesleybruno910@gmail.com>
2023-09-05 09:58:59 -0300

    feat: adds findAtivoById() method to PatientRepository
    Adds findAtivoById() method with a personalized query. The query looks for a patient by Id and return a boolean.

--- src/main/java/med/voll/api/domain/paciente/PatientRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PatientRepository.java
@@ -3,7 +3,11 @@ package med.voll.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Paciente , Long> {
     Page<Paciente> findAll(Pageable pageable);
+
+    @Query("SELECT p.ativo FROM Paciente p WHERE p.id = :id")
+    boolean findAtivoById(Long id);
 }
