d0ce6cc831060ead5e36219efe11fd44c9e2989b
wesley <wesleybruno910@gmail.com>
2023-09-05 09:52:48 -0300

    feat: adds deleteAppointMentIfBefore24Hours(), existsByMedicoIdAndData(), findByPacienteId() methods to ConsultaRepository
    These method allows to make queries with business logic

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,6 +1,19 @@
 package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    @Modifying
+    @Query("DELETE FROM Consulta c WHERE FUNCTION('TIMESTAMPDIFF', HOUR, CURRENT_TIMESTAMP(), c.data) > 24 AND c.id = :id")
+    void deleteAppointmentIfBefore24Hours(Long id);
+
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
+
+    @Query("SELECT c.data FROM Consulta  c WHERE c.paciente.id = :pacienteId")
+    Optional<LocalDateTime> findByPacienteId(Long pacienteId);
 }
